f6424689acd092de5775b10ab78bf9703a8b5853
Martin <Martin@DESKTOP-UAGP239>
2019-10-20 21:27:40 -0400

    Revert "Adds new feature to PieceType enum, for purposes of finding the pieceType from an ID (useful for Command, which takes the piece ID as an argument, as passing Piece objects around individually sounds dangerous)."
    This reverts commit 6e012a44161f37cc9d7c417abc7dbdce65a51239.

--- src/GameInternal/PieceType.java
+++ src/GameInternal/PieceType.java
@@ -1,19 +1,10 @@
 package GameInternal;
 
 public enum PieceType {
-	FOX_EW("Fox"),
-	FOX_NS("Fox"),
-	RABBIT("Rabbit"),
-	HILL("Hill"),
-	HOLE("Hole"),
-	MUSHROOM("Mushroom");
-	
-	private String pieceName;
-	PieceType(String name){
-		this.pieceName = name;
-	}
-	
-	public String getName() {
-		return pieceName;
-	}
+	FOX_EW,
+	FOX_NS,
+	RABBIT,
+	HILL,
+	HOLE,
+	MUSHROOM
 }
