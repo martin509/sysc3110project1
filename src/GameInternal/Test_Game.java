a7066f0b99b9541e7ccb28c72b5a04055357bd5a
Martin <Martin@DESKTOP-UAGP239>
2019-10-21 20:30:51 -0400

    Updates tests for getPiecesOfType test.

--- src/GameInternal/Test_Game.java
+++ src/GameInternal/Test_Game.java
@@ -11,11 +11,24 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({})
 public class Test_Game {
 	@Test
-	void testGameConstructor() {
+	void testGenIDs() {
 		Game game1 = new Game(5, 5);
 		assertEquals("Fox 1", game1.genNewID(PieceType.FOX_EW),"First Fox should be FOX1");
 		assertEquals("Fox 2", game1.genNewID(PieceType.FOX_EW),"2nd Fox should be FOX2");
 		assertEquals("Fox 3", game1.genNewID(PieceType.FOX_NS),"3rd Fox should be FOX3");
 		assertEquals("Rabbit 1", game1.genNewID(PieceType.RABBIT),"1st Rabbit should be RABBIT1");
 	}
+	
+	@Test
+	void testgetPiecesOfType() {
+		Game game1 = new Game(5, 5);
+		assertEquals(true, game1.addPiece(1, 0, PieceType.RABBIT));
+		assertEquals(true, game1.addPiece(1, 1, PieceType.RABBIT));
+		assertEquals(true, game1.addPiece(2, 2, PieceType.RABBIT));
+		assertEquals(true, game1.addPiece(2, 3, PieceType.MUSHROOM));
+		assertEquals(3, game1.getPiecesOfType(PieceType.RABBIT).size(), "Should get 3 pieces.");
+	}
+	/*void testAddPieces() {
+		Game game1 = new Game(5, 5);
+	}*/
 }
